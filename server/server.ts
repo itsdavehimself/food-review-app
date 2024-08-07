d4ae632142cd77bdb4d0957c4c7a131ebef720aa
David <23064280+itsdavehimself@users.noreply.github.com>
2024-08-07 13:19:02 -0500

    Create server connected with MongoDB

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,42 @@
-console.log('hi');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import userAuthRouter from './routes/userAuthRoutes';
+const port = 3000;
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+	cors({
+		origin: ['http://localhost:5173'],
+	})
+);
+
+app.use(express.json());
+app.use(cors({ origin: true, credentials: true }));
+app.use('/api/auth', userAuthRouter);
+
+const mongoURI = process.env.MONGO_URI;
+
+if (!mongoURI) {
+	console.log(
+		'MONGO_URI is not defined. Make sure it is set in your environment variables.'
+	);
+	process.exit(1);
+}
+
+mongoose
+	.connect(mongoURI)
+	.then(() => {
+		console.log('Connected to MongoDB');
+		app.listen(port, '0.0.0.0', () => {
+			console.log(`Server is listening at http://0.0.0.0:${port}`);
+		});
+	})
+	.catch((err) => {
+		console.error('Failed to connect to MongoDB', err);
+		process.exit(1);
+	});
