5bfd3b0e3db69ecb2a379a9b05a4a4055dfc0563
David <23064280+itsdavehimself@users.noreply.github.com>
2024-08-12 19:06:15 -0500

    Consolidate cors options

--- server/server.ts
+++ server/server.ts
@@ -13,12 +13,12 @@ app.use(express.json());
 
 app.use(
 	cors({
-		origin: ['http://localhost:5173'],
+		origin: 'http://localhost:5173',
+		credentials: true,
 	})
 );
 
 app.use(express.json());
-app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
 app.use('/api/auth', userAuthRouter);
 
