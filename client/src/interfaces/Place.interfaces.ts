c7b52e249c35c4a4bbfefe9ce96eb162f3dc1d89
davehimself <itsdavehimself@users.noreply.github.com>
2024-08-14 22:10:26 -0500

    Create User & Place interfaces

---
+++ client/src/interfaces/Place.interfaces.ts
@@ -0,0 +1,22 @@
+export interface Place {
+	displayName: {
+		text: string;
+	};
+	formattedAddress: string;
+	photos: Photo[];
+	priceLevel: string;
+	photoUrl: string;
+}
+
+export interface Photo {
+	authorAttributions: AuthorAttribution[];
+	heightPx: number;
+	name: string;
+	widthPx: number;
+}
+
+export interface AuthorAttribution {
+	displayName: string;
+	photoUri: string;
+	uri: string;
+}
