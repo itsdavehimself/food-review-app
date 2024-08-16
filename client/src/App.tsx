b008010b8f2f18ccd5c1a21455dd04750f36da69
davehimself <itsdavehimself@users.noreply.github.com>
2024-08-16 17:10:27 -0500

    Refactor with new places dispatch functions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,51 +1,80 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SignUp from './containers/authentication/SignUp/SignUp';
-import Dashboard from './containers/Dashboard/Dashboard';
+import Dashboard from './containers/Home/Home';
 import { useAppDispatch, useAppSelector } from './app/hooks';
 import { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
-import { jwtDecode } from 'jwt-decode';
-import { login, logout } from './app/slices/userSlice';
-import { UserInfoPayload } from './components/SignUp/FinishSignUp/FinishSignUp';
+import { logout } from './app/slices/userSlice';
+import handleToken from './helpers/handleToken';
 import Login from './components/Login/Login';
-import refreshAccessToken from './helpers/refreshAccessToken';
 import WithoutNav from './containers/WithoutNav/WithoutNav';
 import WithNav from './containers/WithNav/WithNav';
 import Map from './containers/Map/Map';
 import Explore from './containers/Explore/Explore';
 import Profile from './containers/Profile/Profile';
+import searchNearby from './helpers/searchNearby';
+import { setLocation } from './app/slices/locationSlice';
+import {
+	fetchPlacesFailure,
+	fetchPlacesStart,
+	fetchPlacesSuccess,
+} from './app/slices/placesSlice';
 
 function App() {
 	const dispatch = useAppDispatch();
 	const token = Cookies.get('accessToken');
 	const user = useAppSelector((state) => state.user);
+	const location = useAppSelector((state) => state.location);
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 
 	useEffect(() => {
-		const checkToken = async () => {
-			if (token) {
-				try {
-					const decodedToken = jwtDecode<UserInfoPayload>(token);
-					dispatch(login(decodedToken.UserInfo));
-				} catch (error) {
-					console.error('Failed to decode token', error);
-					const newToken = await refreshAccessToken();
-					if (newToken) {
-						const newDecodedToken = jwtDecode<UserInfoPayload>(newToken);
-						dispatch(login(newDecodedToken.UserInfo));
-					} else {
-						dispatch(logout());
-					}
-				}
-			} else {
+		const checkToken = async (): Promise<void> => {
+			try {
+				await handleToken(token, dispatch);
+			} catch (error) {
 				dispatch(logout());
+			} finally {
+				setIsLoading(false);
 			}
-			setIsLoading(false);
 		};
 
 		checkToken();
 	}, [dispatch, token]);
+
+	useEffect(() => {
+		function success(pos: GeolocationPosition) {
+			const crd = pos.coords;
+			dispatch(
+				setLocation({ latitude: crd.latitude, longitude: crd.longitude })
+			);
+		}
+
+		function error(err: GeolocationPositionError) {
+			console.warn(`ERROR(${err.code}): ${err.message}`);
+		}
+
+		navigator.geolocation.getCurrentPosition(success, error);
+	}, []);
+
+	useEffect(() => {
+		if (location.latitude !== -1 && location.longitude !== -1) {
+			const searchPlacesNearby = async () => {
+				dispatch(fetchPlacesStart());
+
+				try {
+					const nearby = await searchNearby(
+						location.latitude,
+						location.longitude
+					);
+					dispatch(fetchPlacesSuccess(nearby.places));
+				} catch (error) {
+					dispatch(fetchPlacesFailure());
+				}
+			};
+			searchPlacesNearby();
+		}
+	}, [location.latitude, location.longitude, dispatch]);
 
 	if (isLoading) {
 		return <div>Loading...</div>;
