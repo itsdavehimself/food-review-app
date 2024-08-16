f57a09d2c2f46149c712f44008f843d2b273cc15
davehimself <itsdavehimself@users.noreply.github.com>
2024-08-16 17:08:52 -0500

    Add status key

--- client/src/app/slices/placesSlice.ts
+++ client/src/app/slices/placesSlice.ts
@@ -3,22 +3,32 @@ import { Place } from '../../interfaces/Place.interfaces';
 
 export interface PlaceState {
 	places: Place[];
+	status: 'idle' | 'loading' | 'succeeded' | 'failed';
 }
 
 const initialState: PlaceState = {
 	places: [],
+	status: 'idle',
 };
 
 const placesSlice = createSlice({
 	name: 'places',
 	initialState,
 	reducers: {
-		setPlaces(state, action: PayloadAction<Place[]>) {
+		fetchPlacesStart(state) {
+			state.status = 'loading';
+		},
+		fetchPlacesSuccess(state, action: PayloadAction<Place[]>) {
 			state.places = [...state.places, ...action.payload];
+			state.status = 'succeeded';
+		},
+		fetchPlacesFailure(state) {
+			state.status = 'failed';
 		},
 	},
 });
 
-export const { setPlaces } = placesSlice.actions;
+export const { fetchPlacesStart, fetchPlacesSuccess, fetchPlacesFailure } =
+	placesSlice.actions;
 
 export default placesSlice.reducer;
