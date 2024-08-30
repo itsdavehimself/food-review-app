c059ea0c89119fb5c5a5e9505f317bd1a91a070e
davehimself <itsdavehimself@users.noreply.github.com>
2024-08-29 21:13:37 -0500

    Remove state object from location

--- client/src/containers/Explore/Explore.tsx
+++ client/src/containers/Explore/Explore.tsx
@@ -2,10 +2,17 @@ import styles from './Explore.module.scss';
 import Searchbar from '../../components/Searchbar/Searchbar';
 import RestaurantSearchCard from '../../components/RestaurantSearchCard/RestaurantSearchCard';
 import { useAppSelector } from '../../app/hooks';
+import { useNavigate } from 'react-router-dom';
+import { Place } from '../../interfaces/Place.interfaces';
 
 const Explore: React.FC = () => {
 	const places = useAppSelector((state) => state.places.places);
 	const isLoading = useAppSelector((state) => state.places.status);
+	const navigate = useNavigate();
+
+	const handleClick = (place: Place) => {
+		navigate(`/restaurant/${place.id}`);
+	};
 
 	return (
 		<>
@@ -14,14 +21,13 @@ const Explore: React.FC = () => {
 				<h2>Popular Spots Near You</h2>
 				{isLoading === 'succeeded' ? (
 					<>
-						{' '}
 						{places &&
 							places.map((place) => (
 								<RestaurantSearchCard
 									key={place.id}
-									id={place.id}
 									name={place.displayName.text}
 									address={place.formattedAddress}
+									onClick={() => handleClick(place)}
 								/>
 							))}
 					</>
