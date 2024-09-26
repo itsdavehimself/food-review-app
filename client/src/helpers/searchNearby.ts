e2e7d1d29d8ade45614d52144955d1e8317d2854
davehimself <itsdavehimself@users.noreply.github.com>
2024-09-25 20:45:02 -0500

    Move getPhoto fn to own component

--- client/src/helpers/searchNearby.ts
+++ client/src/helpers/searchNearby.ts
@@ -1,3 +1,5 @@
+import { Place } from '../interfaces/Place.interfaces';
+
 async function searchNearby(latitude: number, longitude: number) {
 	const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
 
@@ -43,37 +45,17 @@ async function searchNearby(latitude: number, longitude: number) {
 		// const placesWithPhoto = await Promise.all(
 		// 	data.places.map(async (place: Place) => {
 		// 		if (place.photos && place.photos.length > 0) {
-		// 			const firstPhoto = place.photos[0];
-		// 			const photoUrl = await getPhoto(firstPhoto.name);
-		// 			return { ...place, photoUrl };
+		// 			const firstPhotoName = place.photos[0].name;
+		// 			console.log(firstPhotoName);
+		// 			return { ...place, firstPhotoName };
 		// 		}
 		// 		return place;
 		// 	})
 		// );
-
 		return data;
 	} catch (error) {
 		console.error('Error fetching nearby places:', error);
 	}
 }
-
-// async function getPhoto(photoName: string) {
-// 	const apiKey = 'AIzaSyDSF4KK43ZEU0Zsq7je2GVhg6iekuyMs68';
-// 	const url = `https://places.googleapis.com/v1/${photoName}/media?maxHeightPx=500&key=${apiKey}`;
-
-// 	try {
-// 		const response = await fetch(url, {
-// 			method: 'GET',
-// 		});
-
-// 		if (!response.ok) {
-// 			throw new Error('Network response was not ok');
-// 		}
-
-// 		return response.url;
-// 	} catch (error) {
-// 		console.error('Error fetching photo:', error);
-// 	}
-// }
 
 export default searchNearby;
