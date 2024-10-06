a61d85d300a178e4ea6ab48f90d07af070e329a6
davehimself <itsdavehimself@users.noreply.github.com>
2024-10-06 15:44:01 -0500

    Fix multiple re-renders

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SignUp from './containers/authentication/SignUp/SignUp';
 import Dashboard from './containers/Home/Home';
 import { useAppDispatch, useAppSelector } from './app/hooks';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import Cookies from 'js-cookie';
 import { logout } from './app/slices/userSlice';
 import handleToken from './helpers/handleToken';
@@ -22,43 +22,51 @@ import {
 } from './app/slices/placesSlice';
 import Restaurant from './containers/Restaurant/Restaurant';
 import { LoadScript } from '@react-google-maps/api';
+import Preferences from './containers/Preferences/Preferences';
 
 function App() {
 	const mapsApiKey = import.meta.env.VITE_GOOGLE_API_KEY;
 	const dispatch = useAppDispatch();
-	const token = Cookies.get('accessToken');
+	const [token, setToken] = useState<string | undefined>(
+		Cookies.get('accessToken')
+	);
 	const user = useAppSelector((state) => state.user);
 	const location = useAppSelector((state) => state.location);
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 
-	useEffect(() => {
-		const checkToken = async (): Promise<void> => {
-			try {
+	const initializeApp = useCallback(async (): Promise<void> => {
+		try {
+			if (token) {
 				await handleToken(token, dispatch);
-			} catch (error) {
-				dispatch(logout());
-			} finally {
-				setIsLoading(false);
+			} else {
+				const storedToken = Cookies.get('accessToken');
+				if (storedToken) {
+					setToken(storedToken);
+					await handleToken(storedToken, dispatch);
+				}
 			}
-		};
 
-		checkToken();
-	}, [dispatch, token]);
+			navigator.geolocation.getCurrentPosition(
+				(pos) => {
+					const crd = pos.coords;
+					dispatch(
+						setLocation({ latitude: crd.latitude, longitude: crd.longitude })
+					);
+				},
+				(err) => {
+					console.warn(`ERROR(${err.code}): ${err.message}`);
+				}
+			);
+		} catch (error) {
+			dispatch(logout());
+		} finally {
+			setIsLoading(false);
+		}
+	}, [token, dispatch]);
 
 	useEffect(() => {
-		function success(pos: GeolocationPosition) {
-			const crd = pos.coords;
-			dispatch(
-				setLocation({ latitude: crd.latitude, longitude: crd.longitude })
-			);
-		}
-
-		function error(err: GeolocationPositionError) {
-			console.warn(`ERROR(${err.code}): ${err.message}`);
-		}
-
-		navigator.geolocation.getCurrentPosition(success, error);
-	}, []);
+		initializeApp();
+	}, [initializeApp]);
 
 	useEffect(() => {
 		if (location.latitude !== -1 && location.longitude !== -1) {
@@ -75,6 +83,7 @@ function App() {
 					dispatch(fetchPlacesFailure());
 				}
 			};
+
 			searchPlacesNearby();
 		}
 	}, [location.latitude, location.longitude, dispatch]);
@@ -88,41 +97,91 @@ function App() {
 			<LoadScript googleMapsApiKey={mapsApiKey} libraries={['places']}>
 				<BrowserRouter>
 					<Routes>
-						<Route>
-							<Route element={<WithoutNav />}>
-								<Route
-									path="/signup"
-									element={!user.username ? <SignUp /> : <Navigate to="/" />}
-								/>
-								<Route
-									path="/login"
-									element={!user.username ? <Login /> : <Navigate to="/" />}
-								/>
-							</Route>
+						<Route element={<WithoutNav />}>
+							<Route
+								path="/signup"
+								element={!user.username ? <SignUp /> : <Navigate to="/" />}
+							/>
+							<Route
+								path="/login"
+								element={!user.username ? <Login /> : <Navigate to="/" />}
+							/>
 						</Route>
 						<Route element={<WithNav />}>
 							<Route
 								path="/"
 								element={
-									user.username ? <Dashboard /> : <Navigate to="/login" />
+									user.username ? (
+										user.userPreferencesSet ? (
+											<Dashboard />
+										) : (
+											<Navigate to="/preferences" />
+										)
+									) : (
+										<Navigate to="/login" />
+									)
+								}
+							/>
+							<Route
+								path="/preferences"
+								element={
+									user.username ? <Preferences /> : <Navigate to="/login" />
 								}
 							/>
 							<Route
 								path="/map"
-								element={user.username ? <Map /> : <Navigate to="/login" />}
+								element={
+									user.username ? (
+										user.userPreferencesSet ? (
+											<Map />
+										) : (
+											<Navigate to="/preferences" />
+										)
+									) : (
+										<Navigate to="/login" />
+									)
+								}
 							/>
 							<Route
 								path="/explore"
-								element={user.username ? <Explore /> : <Navigate to="/login" />}
+								element={
+									user.username ? (
+										user.userPreferencesSet ? (
+											<Explore />
+										) : (
+											<Navigate to="/preferences" />
+										)
+									) : (
+										<Navigate to="/login" />
+									)
+								}
 							/>
 							<Route
 								path="/user/:username"
-								element={user.username ? <Profile /> : <Navigate to="/login" />}
+								element={
+									user.username ? (
+										user.userPreferencesSet ? (
+											<Profile />
+										) : (
+											<Navigate to="/preferences" />
+										)
+									) : (
+										<Navigate to="/login" />
+									)
+								}
 							/>
 							<Route
 								path="/restaurant/:placeId"
 								element={
-									user.username ? <Restaurant /> : <Navigate to="/login" />
+									user.username ? (
+										user.userPreferencesSet ? (
+											<Restaurant />
+										) : (
+											<Navigate to="/preferences" />
+										)
+									) : (
+										<Navigate to="/login" />
+									)
 								}
 							/>
 						</Route>
