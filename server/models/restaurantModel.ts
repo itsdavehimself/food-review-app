4a13b19945139d12d6d4cf9be3aeeda37cf11d72
davehimself <itsdavehimself@users.noreply.github.com>
2024-10-06 15:37:12 -0500

    Update restaurant model

--- server/models/restaurantModel.ts
+++ server/models/restaurantModel.ts
@@ -1,14 +1,15 @@
 import mongoose, { Schema } from 'mongoose';
 import RestaurantDocument from '../documents/restaurantDocument';
 
-const restaurantSchema = new mongoose.Schema({
-	googlePlaceId: { type: String, required: true, unique: true },
-	name: { type: String, required: true, unique: false },
-	address: { type: String, required: true, unique: false },
-	photoUrl: { type: String, required: true, unique: true },
-	reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
-	createdAt: { type: Date, default: Date.now },
-});
+const restaurantSchema = new mongoose.Schema(
+	{
+		googlePlaceId: { type: String, required: true, unique: true },
+		name: { type: String, required: true, unique: false },
+		address: { type: String, required: true, unique: false },
+		photoUrl: { type: String, required: true, unique: true },
+	},
+	{ timestamps: true }
+);
 
 const Restaurant = mongoose.model<RestaurantDocument>(
 	'Restaurant',
